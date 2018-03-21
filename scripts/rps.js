fed40ad62d7e011b90240787d4b99e0c16bb4e63
EdMagal <ed83isme@gmail.com>
2018-03-21 16:44:24 -0300

    Update rps.js

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,53 +1,47 @@
-const SCORE = [0, 0];
+  // cache variables
+  const CHOICES = ["rock", "paper", "scissors"];
+  const SCORE = [0, 0];
+  var msg = document.getElementById("msg");
+  var wrapper = document.getElementById("wrapper");
+  var playAgain = document.getElementById("again");
 
-function computerPlay() {
-  // assigns a random numbers 0-2 to represent the computer's choice.
-  let computerSelection = Math.floor(Math.random() * 3);
-  return ["Rock", "Paper", "Scissors"][computerSelection];
-}
+  function playRound(playerSelection) {
 
-function playRound(playerSelection, computerSelection) {
-	// fix the casing of the inputs and removes weird characters
-	playerSelection = playerSelection[0].toUpperCase() +
-			     playerSelection.substr(1)
-					    .toLowerCase()
-					    .replace(/\W|\d/g,"");
-  
-  const CHOICES = ["Rock", "Paper", "Scissors"];
-  
-  // checks if the player has entered a valid choice.
-  if (CHOICES.indexOf(playerSelection) === -1) {
-  	return `Please enter "Rock", "Paper" or "Scissors"`;
+    var randomnizer = Math.floor(Math.random() * 3);
+    var computerSelection = CHOICES[randomnizer];
+
+    // Runs when it is a draw.
+    if (playerSelection === computerSelection) {
+      msg.innerHTML = `It is a draw! Score: ${SCORE[0]}-${SCORE[1]}`;
+
+      // Runs when the player has won the round.
+    } else if (
+      playerSelection === "rock" && computerSelection === "scissors" ||
+      playerSelection === "paper" && computerSelection === "rock" ||
+      playerSelection === "scissors" && computerSelection === "paper") {
+      SCORE[0]++;
+      msg.innerHTML = (`You win! ${playerSelection} beats ${computerSelection}! Score: ${SCORE[0]}-${SCORE[1]}`);
+
+      // Runs when the computer has won the round.
+    } else {
+      SCORE[1]++;
+      msg.innerHTML = (`You lose! ${computerSelection} beats ${playerSelection}! Score: ${SCORE[0]}-${SCORE[1]}`);
+    }
+
+    if (SCORE[0] > 2 || SCORE[1] > 2) {
+      wrapper.style = "display: none";
+      playAgain.style = "display: block";
+      SCORE[0] > 2 ?
+        msg.innerHTML = (`YOU ARE THE CHAMPION!<br /> Final score: ${SCORE[0]}-${SCORE[1]}`) :
+        msg.innerHTML = (`You lost. Come back when you've become stronger.<br /> Final score: ${SCORE[0]}-${SCORE[1]}`);
+    }
+
   }
-                    
-  // Runs when it is a draw.
-  if (playerSelection === computerSelection) {
-    return `It is a draw! Score: ${SCORE[0]}-${SCORE[1]}`;
 
-  // Runs when the player has won the round.
-  } else if (
-    playerSelection === "Rock"     && computerSelection === "Scissors" ||
-    playerSelection === "Paper"    && computerSelection === "Rock"     ||
-    playerSelection === "Scissors" && computerSelection === "Paper"      ) {
-    SCORE[0]++;
-    return `You win! ${playerSelection} beats ${computerSelection}! Score: ${SCORE[0]}-${SCORE[1]}`;
-	  
-  // Runs when the computer has won the round.
-  } else {
-    SCORE[1]++;
-    return `You lose! ${computerSelection} beats ${playerSelection}! Score: ${SCORE[0]}-${SCORE[1]}`;
+  function again() {
+    SCORE[0] = 0;
+    SCORE[1] = 0;
+    msg.innerHTML = "Please, select your move!";
+    playAgain.style = "display: none";
+    wrapper.style = "display: block";
   }
-}
-
-// best out of 5 matches
-while (SCORE[0] < 3 && SCORE[1] < 3) {
-  const playerSelection = window.prompt("Rock, Paper, or Scissors?", "rock")
-  const computerSelection = computerPlay();
-
-  console.log(playRound(playerSelection, computerSelection))
-}
-
-// final winner declaration.
-SCORE[0]>SCORE[1] ? 
-console.log("YOU ARE THE CHAMPION!") : 
-console.log("You lost. Come back when you've become stronger.");
